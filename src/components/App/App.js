d8f8a171383ee4940aa9d3946622190d11027588
Rachel Buchta <rachelbuchta@yahoo.com>
2021-03-04 16:57:17 -0700

    Add utilities file and import landing page into app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
-
+import React from 'react'
 import './App.css';
+import LandingPage from '../LandingPage/LandingPage'
 
 function App() {
   return (
     <div className="App">
-    hey
+    App
+    <LandingPage />
 
     </div>
   );
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './LandingPage.css'
+
+function LandingPage() {
+
+  return(
+    <h1>LandingPage</h1>
+  )
+}
+
+export default LandingPage
