f46f1d7cb87f30efe4288fa44f3d135ca58c342a
Rachel Buchta <rachelbuchta@yahoo.com>
2021-03-04 19:01:07 -0700

    Import background, add intro text and button

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,12 +2,15 @@ import React from 'react'
 import './LandingPage.css'
 import teacup from '../../assets/tea.svg'
 
+
 function LandingPage() {
 
   return (
       <main>
-        <h1>positiviTEA.</h1>
+        <h1 className='header'>positiviTEA.</h1>
         <img className='teacup' alt='teacup' src={teacup}/> 
+        <p className='intro-text'>Get the tea on all things good and interesting happening in the world.</p>
+        <button className='spill-button'>Spill it..</button>
       </main>
   )
 }
