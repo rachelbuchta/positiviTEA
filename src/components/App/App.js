dc381047b4a00292ff0744439778531d9ae693c0
Rachel Buchta <rachelbuchta@yahoo.com>
2021-03-04 17:04:48 -0700

    Import fetch calls into app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './App.css'
 import LandingPage from '../LandingPage/LandingPage'
+import {fetchNews, fetchQuotes} from '../../utilities'
 
 function App() {
+
+
   return (
     <div className="App">
     App
