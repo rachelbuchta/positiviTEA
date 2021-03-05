3375164c7467a3e8acc776968bafdc6264a8dafc
Rachel Buchta <rachelbuchta@yahoo.com>
2021-03-05 11:15:38 -0700

    Add more styling to header and story of the day page

--- src/components/StoryOfTheDay/StoryOfTheDay.js
+++ src/components/StoryOfTheDay/StoryOfTheDay.js
@@ -27,18 +27,23 @@ function StoryOfTheDay({news, grabNews}) {
     // </article>
     <article className='story'>
       <h2 className='sod'>Story Of The Day</h2>
-      <section className='imgWrapper'>
-        <img className='newsImage' src={earth}/>
-        <p className='storyTitle'>This is the title of the article</p>
-      </section>
-      <section className='dateAndLabel'>
-        <p className='date'>2020-03-21</p>
-        <p className='label'>Inspiring</p>
-      </section>
-      <section className='descriptionWrapper'>
-        <p className='description'>This is a brief desciption of what the news story will be about to grab a potential readers attention. Better be captivating as fuck</p>
-        <button className='viewMore'>See Full Story</button>
-      </section>
+      <div className='contentWrapper'>
+        <section className='imgWrapper'>
+          <img className='newsImage' src={earth}/>
+          <p className='storyTitle'>This is the title of the article</p>
+        </section>
+        <section className='dateAndLabel'>
+          <p className='date'>2020-03-21</p>
+          <p className='label'>Inspiring</p>
+        </section>
+        <section className='descriptionWrapper'>
+          <span>
+
+            <p className='description'>This is a brief desciption of what the news story will be about to grab a potential readers attention. Better be captivating as fuck</p>
+            <button className='viewMore'>See Full Story</button>
+          </span>
+        </section>
+      </div>
       <section className='navBtns'>
         <button className='favorite'>&hearts;</button>
         <button onClick={grabNews} className='spillAgain'>Spill it again..</button>
