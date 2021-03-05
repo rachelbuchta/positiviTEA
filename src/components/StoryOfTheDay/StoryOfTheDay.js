7168d33b6db42721d189cd12af6f9825f757ad7c
Rachel Buchta <rachelbuchta@yahoo.com>
2021-03-05 00:33:40 -0700

    Render all elements from news story and quote to story page

--- src/components/StoryOfTheDay/StoryOfTheDay.js
+++ src/components/StoryOfTheDay/StoryOfTheDay.js
@@ -4,9 +4,26 @@ import './StoryOfTheDay.css'
 function StoryOfTheDay({news}) {
 
   return (
-    <section className='story'>
-      <h1>{news.author}</h1>
-    </section>
+    <article className='story'>
+      <h2 className='sod'>Story Of The Day</h2>
+      <section className='imgWrapper'>
+        <img src={news.urlToImage}/>
+        <p className='storyTitle'>{news.title}</p>
+      </section>
+      <section className='dateAndLabel'>
+        <p className='date'>{news.publishedAt}</p>
+        <p className='label'>Inspiring</p>
+      </section>
+      <section className='descriptionWrapper'>
+        <p className='description'>{news.description}</p>
+        <button className='viewMore'>See Full Story</button>
+      </section>
+      <section className='navBtns'>
+        <button className='favorite'>&hearts;</button>
+        <button className='spillAgain'>Spill it again..</button>
+        <button className='viewSaved'>View Saved</button>
+      </section>
+    </article>
   )
 
 }
