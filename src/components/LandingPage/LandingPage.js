510ce30d3ee273202c4ca5d4d85a5dbf530d24d2
Rachel Buchta <rachelbuchta@yahoo.com>
2021-03-04 19:53:01 -0700

    Add random number function to generate a random news story from array

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -3,14 +3,15 @@ import './LandingPage.css'
 import teacup from '../../assets/tea.svg'
 
 
-function LandingPage() {
+
+function LandingPage({grabAllData}) {
 
   return (
       <main>
         <h1 className='header'>positiviTEA.</h1>
         <img className='teacup' alt='teacup' src={teacup}/> 
         <p className='intro-text'>Get the tea on all things good and interesting happening in the world.</p>
-        <button className='spill-button'>Spill it..</button>
+        <button onClick={grabAllData} className='spill-button'>Spill it..</button>
       </main>
   )
 }
