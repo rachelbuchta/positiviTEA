ae2cda70dab1aa69a06acd717fdb88b54c287a6b
Rachel Buchta <rachelbuchta@yahoo.com>
2021-03-06 19:48:58 -0700

    Import icon to add functionality to delete stories from favorites

--- src/components/FavoritedCards/FavoritedCards.js
+++ src/components/FavoritedCards/FavoritedCards.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './FavoritedCards.css'
+import remove from '../../assets/cancel.png'
 
 const FavoritedCards = ({title, image, url}) => {
   return (
-    <article className='card'>
-      <p className='savedTitle'>{title}</p>
-      <img className='image' src={image}/>
-      {/* <p>{url}</p> */}
-    </article>
+    <section className='cardAndDelete'>
+      <img className='remove' src={remove} alt='delete story' />
+      <article className='card'>
+        <p className='savedTitle'>{title}</p>
+        <img className='image' src={image}/>
+        {/* <p>{url}</p> */}
+      </article>
+    </section>
   )
 }
 
